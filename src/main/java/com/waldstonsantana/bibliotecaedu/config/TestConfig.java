918685dbc2fd19ea4a348471e73241271c857733
Waldston Santana <waldston.ws@gmail.com>
2024-02-12 23:24:26 -0300

    Atualização na classe TestConfig

--- src/main/java/com/waldstonsantana/bibliotecaedu/config/TestConfig.java
+++ src/main/java/com/waldstonsantana/bibliotecaedu/config/TestConfig.java
@@ -1,6 +1,9 @@
 package com.waldstonsantana.bibliotecaedu.config;
 
 
+import com.waldstonsantana.bibliotecaedu.models.AutorModel;
+import com.waldstonsantana.bibliotecaedu.repositories.AutorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -9,9 +12,14 @@ import org.springframework.context.annotation.Profile;
 @Profile("test")
 public class TestConfig implements CommandLineRunner {
 
+    @Autowired
+    private AutorRepository autorRepository;
 
     @Override
     public void run(String... args) throws Exception {
 
+        AutorModel joao = new AutorModel("João Gomes", "Masculino", "2000","05415624567");
+
+        autorRepository.save(joao);
     }
 }
