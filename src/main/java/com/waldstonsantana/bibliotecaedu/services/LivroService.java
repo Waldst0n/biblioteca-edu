053e1572b5845749884f1f1971066d3478923c61
Waldston Santana <waldston.ws@gmail.com>
2024-02-13 09:55:24 -0300

    Classe LivroService

---
+++ src/main/java/com/waldstonsantana/bibliotecaedu/services/LivroService.java
@@ -0,0 +1,25 @@
+package com.waldstonsantana.bibliotecaedu.services;
+
+import com.waldstonsantana.bibliotecaedu.models.LivroModel;
+import com.waldstonsantana.bibliotecaedu.models.LocatarioModel;
+import com.waldstonsantana.bibliotecaedu.repositories.LivroRepository;
+import com.waldstonsantana.bibliotecaedu.repositories.LocatarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class LivroService {
+
+
+    @Autowired
+    private LivroRepository livroRepository;
+
+    public List<LivroModel> findAll() {
+        return livroRepository.findAll();
+
+    }
+
+}
