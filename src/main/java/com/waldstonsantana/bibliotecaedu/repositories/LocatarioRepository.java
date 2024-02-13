c403fcdd8d2caa079644820b97da6ee3dd58965d
Waldston Santana <waldston.ws@gmail.com>
2024-02-13 08:53:54 -0300

    Classe LocatarioRepository

---
+++ src/main/java/com/waldstonsantana/bibliotecaedu/repositories/LocatarioRepository.java
@@ -0,0 +1,8 @@
+package com.waldstonsantana.bibliotecaedu.repositories;
+
+import com.waldstonsantana.bibliotecaedu.models.LocatarioModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LocatarioRepository extends JpaRepository<LocatarioModel, Long> {
+
+}
