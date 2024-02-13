5dbcf5a4178c15e073d05cc0875aafa5b77a49aa
Waldston Santana <waldston.ws@gmail.com>
2024-02-12 23:23:30 -0300

    Classe AutorRepository

---
+++ src/main/java/com/waldstonsantana/bibliotecaedu/repositories/AutorRepository.java
@@ -0,0 +1,8 @@
+package com.waldstonsantana.bibliotecaedu.repositories;
+
+import com.waldstonsantana.bibliotecaedu.models.AutorModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AutorRepository extends JpaRepository<AutorModel, String> {
+
+}
