de0a30b9ed6466760698e947a659ce83504f93c8
Waldston Santana <waldston.ws@gmail.com>
2024-02-13 09:55:33 -0300

    Classe LivroResource

---
+++ src/main/java/com/waldstonsantana/bibliotecaedu/resources/LivroResource.java
@@ -0,0 +1,28 @@
+package com.waldstonsantana.bibliotecaedu.resources;
+
+import com.waldstonsantana.bibliotecaedu.models.LivroModel;
+import com.waldstonsantana.bibliotecaedu.models.LocatarioModel;
+import com.waldstonsantana.bibliotecaedu.services.LivroService;
+import com.waldstonsantana.bibliotecaedu.services.LocatarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/livros")
+public class LivroResource {
+
+    @Autowired
+    private LivroService livroService;
+
+    @GetMapping
+    public ResponseEntity<List<LivroModel>> findAll() {
+        List<LivroModel> list = livroService.findAll();
+
+        return ResponseEntity.ok().body(list);
+    }
+}
