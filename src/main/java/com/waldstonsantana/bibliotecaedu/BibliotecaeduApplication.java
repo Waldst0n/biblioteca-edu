59b8cbde6c546aed53d1e154304f4b42a372b40a
Waldston Santana <waldston.ws@gmail.com>
2024-02-12 22:57:39 -0300

    Primeiro Commit e config application.properties

---
+++ src/main/java/com/waldstonsantana/bibliotecaedu/BibliotecaeduApplication.java
@@ -0,0 +1,13 @@
+package com.waldstonsantana.bibliotecaedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaeduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaeduApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/waldstonsantana/bibliotecaedu/BibliotecaeduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waldstonsantana.bibliotecaedu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaeduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
